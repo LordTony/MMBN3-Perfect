8ed5ffa41ac60f243e0dd29822aabedbab831d89
LordTony <lordtonysama@gmail.com>
2024-07-04 16:16:47 -0500

    Hooking up the art for the other giga chips

--- patches/nav_recycle.asm
+++ patches/nav_recycle.asm
@@ -1,35 +1,91 @@
-//-------------------------------------
-//   Giga Chips
-//   - Nav Recycle for now
-//-------------------------------------
+////////////////////
+//   Giga Chips   //
+////////////////////////////////////////
+//                                    //
+//   - Nav Recycle                    //
+//   - All art is now in the game     //
+//                                    //
+////////////////////////////////////////
 
-// Override the giga chip anti-cheat no-no list
-.org 0x08001a86
-	.fill 26, 0xff
+//
+// Adding art from white version
+//
 
-// Add Nav Recycle to Undernet 2 Bugfrag Vendor Item List
-.org 0x08044e60
-	.byte 0x02			// Type (0 = Nothing, 1 = Item, 2 = BattleChip, 3 = NaviCust Program)
-	.byte 0x01			// (0xFF = unlimited)
-	.halfword 0x0130	// Item/BattleChip/Program index (16-bit)
-	.byte 0x1a			// Chip code/Program color (8-bit)
-	.byte 0x00			// Unused byte
-	.halfword 0x00c8	// Price in 100z/1 BugFrag (16-bit)
-
-// add the art for the giga chips past the end of the rom
-// Sprite order NavRcycl, AlphArmΣ, Bass, Serenade, Balance
-// 0x700 length for each sprite
-// 0x20 length for each palette
 .definelabel gigchip_data_address, 0x08800000
 .org gigchip_data_address
-	.incbin "white_giga_chip_sprites.dat"
+    .incbin "white_giga_chip_sprites.dat"
 
-// Fix the library page ordering
-.org 0x08013b1e			// This is for Nav Recycle 
-	.halfword 0x00ff	// Set the ordering
-	.skip 0x08
-	.word gigchip_data_address	// Set the art
-	.word gigchip_data_address + 0x700
+/////////////////////////////
+//   Chip Data Structure   //
+//////////////////////////////////////////////////////////
+//                                                      //
+//                             Library Display Order    //
+//                                             |---|    //
+//   00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f    //
+//                                                      //
+//                           Art Addr    Palette Addr   //
+//                           |---------| |---------|    //
+//   10 11 12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f    //
+//                                                      //
+//////////////////////////////////////////////////////////
 
-// TODO: should be able to easily get the other
-// giga chips connected to their art and library sorting
+///////////////
+//   Order   //
+////////////////////////
+//                    //
+//   0 = NavRcycl *   //
+//   1 = AlphArm V    //
+//   2 = Bass X       //
+//   3 = Serenade S   //
+//   4 = Balance Y    //
+//                    //
+////////////////////////
+
+//
+// Hooking up the art to the giga chip data
+// Replacing the "place holder" art image
+//
+
+.definelabel start_of_chip_data, 0x08013b10
+.definelabel chip_art_size, 0x700
+.definelabel chip_art_and_palette_size, 0x720
+
+.macro write_chip_data, index
+    .org start_of_chip_data + (0x20 * index)
+        .skip 0xe
+        .halfword (0x00f0 + index)
+        .skip 0x8
+        .word gigchip_data_address + (chip_art_and_palette_size * index)
+        .word gigchip_data_address + (chip_art_and_palette_size * index) + chip_art_size
+.endmacro
+
+write_chip_data 0
+write_chip_data 1
+write_chip_data 2
+write_chip_data 3
+write_chip_data 4
+
+//////////////////
+//              //
+//   NavRcycl   //
+//              //
+//////////////////
+
+//
+// Override the giga chip anti-cheat no-no list for vendors
+//
+
+.org 0x08001a86
+    .fill 26, 0xff
+
+//
+// Add NavRcycl to Undernet 2 Bugfrag Vendor Item List
+//
+
+.org 0x08044e60
+    .byte 0x02          // Type (0 = Nothing, 1 = Item, 2 = BattleChip, 3 = NaviCust Program)
+    .byte 0x01          // (0xFF = unlimited)
+    .halfword 0x0130    // Item/BattleChip/Program index (16-bit)
+    .byte 0x1a          // Chip code/Program color (8-bit)
+    .byte 0x00          // Unused byte
+    .halfword 0x00c8    // Price in 100z/1 BugFrag (16-bit)
